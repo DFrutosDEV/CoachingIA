2ba861b60a97950340b455b224fc4aa4567dc0c1
DANIEL FRUTOS <dfrutos.dev@gmail.com>
2025-04-22 00:46:11 -0300

    feat: implementacion de PWA para proyecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '../components/theme-provider'
 import { MUIProvider } from '../components/mui-provider'
+import { register } from './register-sw'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -15,6 +16,17 @@ export const metadata: Metadata = {
   title: 'Coaching IA',
   description: 'Coaching IA',
   generator: 'coaching-ia',
+  manifest: '/manifest.json',
+  themeColor: '#000000',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Coaching IA',
+  },
+  formatDetection: {
+    telephone: false,
+  },
 }
 
 export default function RootLayout({
@@ -22,8 +34,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  React.useEffect(() => {
+    register();
+  }, []);
+
   return (
     <html lang="es" suppressHydrationWarning className={inter.variable}>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        -- MODIFICAR ICONO DE APLICACION
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
